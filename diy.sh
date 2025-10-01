93defa2b7f9799d3e5d6dee837b41b1ffa41cd76
xiao-jie-xiao <xiaojiexiaojie813@gmail.com>
2025-10-01 10:40:42 +0800

    Update diy.sh

--- diy.sh
+++ diy.sh
@@ -1,4 +1,1 @@
-# 请在下方输入自定义命令(一般用来安装第三方插件)(可以留空)
-# Please enter the custom command below (usually used to install third-party plugins) (can be left blank)
-# git clone --depth=1 https://github.com/EOYOHOO/UA2F.git package/UA2F
-# git clone --depth=1 https://github.com/EOYOHOO/rkp-ipid.git package/rkp-ipid
+sudo bash -c 'bash <(curl -s https://build-scripts.immortalwrt.org/init_build_environment.sh)'
